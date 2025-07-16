946c327b00c200682a3d3014179383214f35e91b
Rajputujjwal4664 <rajputujjwal4664@gmail.com>
2025-07-16 19:44:56 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,9 +3,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyCCx4QaXdzuUrZki39zJPZH9xXYUgmt2cY",
   authDomain: "belcha-gang.firebaseapp.com",
   projectId: "belcha-gang",
-  storageBucket: "belcha-gang.firebasestorage.app",
+  storageBucket: "belcha-gang.firebasestorage.com",
   messagingSenderId: "762357625552",
-  appId: "1:762357625552:web:8a47b837b19ea7ad0864d5"
+  appId: "1:762357625552:web:8a47b837b19ea7ad0864d5",
+  databaseURL: "https://belcha-gang-default-rtdb.firebaseio.com"
 };
 
 // ✅ Firebase ko initialize karo
